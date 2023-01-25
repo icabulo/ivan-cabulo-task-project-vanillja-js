00aece74feefbc2f34ddc57dc174959ce0a74179
Ivan <icabulo@gmail.com>
2023-01-24 22:22:38 -0500

    paso 3: estructura de archivos

---
+++ js/index.js
@@ -0,0 +1,8 @@
+/* const btn = document.querySelector(".click-me");
+
+function displayAlert() {
+  //   console.log("you have clicked the button");
+  alert("you have clicked the button");
+}
+
+btn.addEventListener("click", displayAlert); */
